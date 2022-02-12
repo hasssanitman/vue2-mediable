b95925aa9368c4afc126c84436cf0091cff11062
Hasan Mohamadian <h.mohamadian89@gmail.com>
2022-02-13 00:06:47 +0330

    Initialize

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { Vue2MediaLibrarySample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  Vue2MediaLibrarySample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue2-media-library-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as Vue2MediaLibrarySample } from './vue2-media-library-sample.vue';
